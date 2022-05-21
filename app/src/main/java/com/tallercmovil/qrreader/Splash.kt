ebfec1b42c0a8ab0ca6669f05b8b26a004eb8759
ellipsiis <danielzarco.m@outlook.com>
2022-05-20 20:03:35 -0500

    initial commit: adding splash screen and main activity with banner

---
+++ app/src/main/java/com/tallercmovil/qrreader/Splash.kt
@@ -0,0 +1,19 @@
+package com.tallercmovil.qrreader
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlin.concurrent.thread
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        thread{
+            Thread.sleep(3000)
+            startActivity(Intent(this@Splash, MainActivity::class.java))
+            finish()
+        }
+    }
+}
