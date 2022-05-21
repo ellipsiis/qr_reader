079918d53270063e5a1de68ffc5400bf576940a1
ellipsiis <danielzarco.m@outlook.com>
2022-05-21 15:00:12 -0500

    feat(qr): complete functions

--- app/src/main/java/com/tallercmovil/qrreader/QR.kt
+++ app/src/main/java/com/tallercmovil/qrreader/QR.kt
@@ -15,11 +15,9 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.zxing.Result
-import com.google.zxing.client.result.AddressBookAUResultParser
-import com.google.zxing.client.result.AddressBookParsedResult
-import com.google.zxing.client.result.ResultParser
-import com.google.zxing.client.result.URIParsedResult
+import com.google.zxing.client.result.*
 import me.dm7.barcodescanner.zxing.ZXingScannerView
+import java.lang.Exception
 import java.net.MalformedURLException
 import java.net.URISyntaxException
 import java.net.URL
@@ -60,32 +58,19 @@ class QR : AppCompatActivity(), ZXingScannerView.ResultHandler {
         val scanResult = p0?.text
 
         Log.d("QR_LEIDO",scanResult!!)
-
         try{
-            val list = scanResult.split(":")
-            Log.d("QR_LEIDO","${list}")
-            var smsIntent = Intent(Intent.ACTION_VIEW)
-            smsIntent.setData(Uri.parse("smsto:"+list[1]))
-            smsIntent.putExtra("sms_body",list[2])
-            startActivity(smsIntent)
-            finish()
-
-        }catch(e: URISyntaxException)
-        {
-
-        }
-
-        try{
-//            for url
-            var url = URL(scanResult)
-            var i = Intent(Intent.ACTION_VIEW)
-            i.setData(Uri.parse(scanResult))
-            startActivity(i)
+//            for email intent
+            val listEmail = scanResult.split(":")
+            var emailIntent = Intent(Intent.ACTION_VIEW)
+            emailIntent.setData(Uri.parse("mailto:"))
+            emailIntent.putExtra(Intent.EXTRA_EMAIL,listEmail[2])
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT,listEmail[4])
+            emailIntent.putExtra(Intent.EXTRA_TEXT,listEmail[6])
+            startActivity(emailIntent)
             finish()
 
 
-
-        }catch(e:MalformedURLException){
+        }catch(e: Exception){
             AlertDialog.Builder(this@QR)
                 .setTitle(getString(R.string.error_title_alertdialog))
                 .setMessage(getString(R.string.error_qr_lecture))
@@ -98,7 +83,47 @@ class QR : AppCompatActivity(), ZXingScannerView.ResultHandler {
         }
 
         try{
+//            For reading a sms
+            val list = scanResult.split(":")
+            var smsIntent = Intent(Intent.ACTION_VIEW)
+            smsIntent.setData(Uri.parse("smsto:"+list[1]))
+            smsIntent.putExtra("sms_body",list[2])
+            startActivity(smsIntent)
+            finish()
 
+        }catch(e:Exception){
+            AlertDialog.Builder(this@QR)
+                .setTitle(getString(R.string.error_title_alertdialog))
+                .setMessage(getString(R.string.error_qr_lecture))
+                .setPositiveButton(getString(R.string.positive_button), DialogInterface.OnClickListener { dialogInterface, i ->
+                    dialogInterface.dismiss()
+                    finish()
+                })
+                .create()
+                .show()
+        }
+
+        try{
+//            for url
+            var url = URL(scanResult)
+            var i = Intent(Intent.ACTION_VIEW)
+            i.setData(Uri.parse(scanResult))
+            startActivity(i)
+            finish()
+
+        }catch(e: Exception){
+            AlertDialog.Builder(this@QR)
+                .setTitle(getString(R.string.error_title_alertdialog))
+                .setMessage(getString(R.string.error_qr_lecture))
+                .setPositiveButton(getString(R.string.positive_button), DialogInterface.OnClickListener { dialogInterface, i ->
+                    dialogInterface.dismiss()
+                    finish()
+                })
+                .create()
+                .show()
+        }
+
+        try{
             var parsedResult = ResultParser.parseResult(p0) as AddressBookParsedResult
             var nValue = parsedResult.names
             var nkValue = parsedResult.nicknames
@@ -109,7 +134,7 @@ class QR : AppCompatActivity(), ZXingScannerView.ResultHandler {
 
 
 
-            var intent = Intent(Intent.ACTION_INSERT).apply {
+            val intent = Intent(Intent.ACTION_INSERT).apply {
                 type = ContactsContract.Contacts.CONTENT_TYPE
                 putExtra(ContactsContract.Intents.Insert.PHONETIC_NAME, nkValue)
                 putExtra(ContactsContract.Intents.Insert.NAME, nValue)
@@ -119,11 +144,11 @@ class QR : AppCompatActivity(), ZXingScannerView.ResultHandler {
                 putExtra(ContactsContract.Intents.Insert.PHONE_TYPE,phtValue)
             }
             startActivity(intent)
-            finish()
-        }catch(e:URISyntaxException){
+
+        }catch(e:Exception){
             AlertDialog.Builder(this@QR)
 //                .setTitle(getString(R.string.error_title_alertdialog))
-                .setTitle("VCARD")
+                .setTitle(getString(R.string.error_title_alertdialog))
                 .setMessage(getString(R.string.error_qr_lecture))
                 .setPositiveButton(getString(R.string.positive_button), DialogInterface.OnClickListener { dialogInterface, i ->
                     dialogInterface.dismiss()
