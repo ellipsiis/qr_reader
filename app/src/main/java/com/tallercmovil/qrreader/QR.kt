01a0ff4bb41fd3b4e12e2138830c8900439c4477
ellipsiis <danielzarco.m@outlook.com>
2022-05-21 03:14:45 -0500

    feat(qr): adding vcard function

--- app/src/main/java/com/tallercmovil/qrreader/QR.kt
+++ app/src/main/java/com/tallercmovil/qrreader/QR.kt
@@ -1,14 +1,28 @@
 package com.tallercmovil.qrreader
 
+import android.Manifest
+import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.zxing.Result
+import com.google.zxing.client.result.AddressBookAUResultParser
+import com.google.zxing.client.result.AddressBookParsedResult
+import com.google.zxing.client.result.ResultParser
+import com.google.zxing.client.result.URIParsedResult
 import me.dm7.barcodescanner.zxing.ZXingScannerView
+import java.net.MalformedURLException
+import java.net.URISyntaxException
+import java.net.URL
 
 class QR : AppCompatActivity(), ZXingScannerView.ResultHandler {
 
@@ -47,5 +61,121 @@ class QR : AppCompatActivity(), ZXingScannerView.ResultHandler {
 
         Log.d("QR_LEIDO",scanResult!!)
 
+        try{
+//            for url
+            val url = URL(scanResult)
+            val i = Intent(Intent.ACTION_VIEW)
+            i.setData(Uri.parse(scanResult))
+            startActivity(i)
+            finish()
+
+
+
+        }catch(e:MalformedURLException){
+            AlertDialog.Builder(this@QR)
+                .setTitle(getString(R.string.error_title_alertdialog))
+                .setMessage(getString(R.string.error_qr_lecture))
+                .setPositiveButton(getString(R.string.positive_button), DialogInterface.OnClickListener { dialogInterface, i ->
+                    dialogInterface.dismiss()
+                    finish()
+                })
+                .create()
+                .show()
+        }
+
+        try{
+
+            var parsedResult = ResultParser.parseResult(p0) as AddressBookParsedResult
+            var nValue = parsedResult.names
+            var nkValue = parsedResult.nicknames
+            var eValue = parsedResult.emails
+            var etValue = parsedResult.emailTypes
+            var phValue = parsedResult.phoneNumbers
+            var phtValue = parsedResult.phoneTypes
+
+
+
+            val intent = Intent(Intent.ACTION_INSERT).apply {
+                type = ContactsContract.Contacts.CONTENT_TYPE
+                putExtra(ContactsContract.Intents.Insert.PHONETIC_NAME, nkValue)
+                putExtra(ContactsContract.Intents.Insert.NAME, nValue)
+                putExtra(ContactsContract.Intents.Insert.EMAIL,eValue)
+                putExtra(ContactsContract.Intents.Insert.EMAIL_TYPE,etValue)
+                putExtra(ContactsContract.Intents.Insert.PHONE,phValue)
+                putExtra(ContactsContract.Intents.Insert.PHONE_TYPE,phtValue)
+            }
+            startActivity(intent)
+            finish()
+        }catch(e:MalformedURLException){
+            AlertDialog.Builder(this@QR)
+//                .setTitle(getString(R.string.error_title_alertdialog))
+                .setTitle("VCARD")
+                .setMessage(getString(R.string.error_qr_lecture))
+                .setPositiveButton(getString(R.string.positive_button), DialogInterface.OnClickListener { dialogInterface, i ->
+                    dialogInterface.dismiss()
+                    finish()
+                })
+                .create()
+                .show()
+        }
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checkPermissions()){
+                if(scannerView == null){
+                    scannerView = ZXingScannerView(this@QR)
+                    setContentView(scannerView)
+                }else{
+                    scannerView?.setResultHandler(this@QR)
+                    scannerView?.startCamera()
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scannerView?.stopCamera()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode){
+            camera_permission ->{
+                if(grantResults.isNotEmpty()){
+                    if(grantResults[0]!=PackageManager.PERMISSION_GRANTED){
+                        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                            if(shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+                                AlertDialog.Builder(this@QR)
+                                    .setTitle(getString(R.string.required_permission))
+                                    .setMessage(getString(R.string.camera_permission_message))
+                                    .setPositiveButton(getString(R.string.positive_button),DialogInterface.OnClickListener { dialogInterface, i ->
+                                        requestPermissions(arrayOf(Manifest.permission.CAMERA), camera_permission)
+                                    })
+                                    .setNegativeButton(getString(R.string.negative_button),DialogInterface.OnClickListener { dialogInterface, i ->
+                                        dialogInterface.dismiss()
+                                        finish()
+                                    })
+                                    .create()
+                                    .show()
+                            }else{
+                                Toast.makeText(this@QR,getString(R.string.camera_permission_negative_message),Toast.LENGTH_LONG)
+                                    .show()
+                                finish()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 }
